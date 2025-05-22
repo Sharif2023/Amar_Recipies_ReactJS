718ed4ce24ffc87af72211f386f014061e0c5dfc
Sharif2023 <sharifislam0505@gmail.com>
2025-05-22 23:12:40 +0600

    all

--- Amar_Recipies/tailwind.config.js
+++ Amar_Recipies/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -9,3 +12,6 @@ export default {
   plugins: [],
 }
 
+//npm i @heroicons/react
+//npm install @material-tailwind/react
+//npm install react-router-dom
