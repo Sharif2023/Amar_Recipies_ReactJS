c7bd4d08d5b1ace702a06aac63a28fa9e74a3199
Sharif2023 <sharifislam0505@gmail.com>
2025-05-23 22:25:37 +0600

    header design

---
+++ Amar_Recipe/src/Components/Header.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div>
+            {/* Navbar */}
+            <nav className="bg-black p-2">
+                <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+
+                    <div className="text-white font-bold text-2xl mb-4 lg:mb-0 hover:text-orange-600 hover:cursor-pointer inline-flex items-center gap-2"><img src="src/assets/Amar_Recipe_Header_Logo.svg" className="w-8 h-8" alt="Amar Recipe Header Logo" />Amar Recipe</div>
+
+                    {/* Hamburger menu for small screens */}
+                    <div className="lg:hidden">
+                        <button onClick={toggleMenu} className="text-white focus:outline-none">
+                            <svg
+                                className="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                ></path>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <form className="max-w-lg mx-auto">
+                        <div className="flex">
+                            <button
+                                onClick={() => setDropdownOpen(!dropdownOpen)}
+                                className="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                                type="button"
+                            >
+                                All categories
+                                <svg className="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                                </svg>
+                            </button>
+
+                            {/* Dropdown menu */}
+                            <div
+                                className={`absolute left-0 mt-1 w-44 z-20 bg-white divide-y divide-gray-100 rounded-lg shadow-md dark:bg-gray-700 ${dropdownOpen ? '' : 'hidden'}`}
+                            >
+                                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                                    {["Mockups", "Templates", "Design", "Logos"].map((item) => (
+                                        <li key={item}>
+                                            <button type="button" className="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                {item}
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="relative w-full">
+                                <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+                                <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                    <span className="sr-only">Search</span>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    {/* Navigation links */}
+                    <div className={`lg:flex flex-col lg:flex-row ${isOpen ? 'block' : 'hidden'} lg:space-x-4 lg:mt-0 mt-4 flex flex-col items-center text-l`}>
+                        <a href="/" className="text-white  px-2 py-2 hover:text-orange-600 ">Home</a>
+                        <a href="#Projects" className="text-white  px-2 py-2  hover:text-orange-600">Projects</a>
+                        <a href="/" className="text-white  px-2 py-2  hover:text-orange-600">About</a>
+                        <a href="/" className="text-white  px-2 py-2  hover:text-orange-600">Contact Me</a>
+                    </div>
+                </div>
+
+            </nav >
+
+
+        </div >
+    );
+}
+
+export default Header;
