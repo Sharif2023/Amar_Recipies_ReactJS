712e4e74a72e24f9e6cb869e7e5b7222834ad646
Sharif2023 <sharifislam0505@gmail.com>
2025-06-21 21:02:08 +0600

    rating button added

--- Amar_Recipe/src/Components/RecipeModal.jsx
+++ Amar_Recipe/src/Components/RecipeModal.jsx
@@ -215,12 +215,26 @@ const RecipeModal = ({ isOpen, onClose, recipe }) => {
                         onClick={() => setReportOpen(!reportOpen)}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                     </svg>
-                    <button
-                        onClick={onClose}
-                        className="bg-rose-600 text-white px-4 py-2 rounded hover:bg-rose-700"
-                    >
-                        বন্ধ করুন
-                    </button>
+                    <div className='flex space-x-1'>
+                        <button
+                            onClick={''}
+                            className="flex bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            </svg>
+                            রেটিং করুন
+                        </button>
+                        <button
+                            onClick={onClose}
+                            className="flex bg-rose-600 text-white px-4 py-2 rounded hover:bg-rose-700"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>
+                            বন্ধ করুন
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
